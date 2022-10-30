e8dfa8893d83d44579a313dbcb27f92fb1ca3bab
JakFranceschini <jakson.franceschini@gmail.com>
2022-10-30 13:52:10 -0300

    Versão 1.0 navbar.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let retrairNavbar = document.getElementById('bloco_navbar');
+
+function retrair() {
+    if (retrairNavbar.id == 'bloco_navbar') {
+        retrairNavbar.id = 'retrair_navbar';
+    } else {
+        retrairNavbar.id = 'bloco_navbar';
+    }
+        
+}
